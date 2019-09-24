778b69717f49ec1b87e8879d6ba0f46893a7cb6d
ENDERSONYUNDASANCHEZ <53826353+ENDERSONYUNDASANCHEZ@users.noreply.github.com>
2019-09-24 15:14:23 -0500

    taller

---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,34 @@
+@extends('products.layout')
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> MOSTRAR PRODUCTOS</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>NOMBRE:</strong>
+                {{ $product->nombre }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>PRECIO:</strong>
+                {{ $product->precio }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>DETALLE:</strong>
+                {{ $product->detalle }}
+            </div>
+        </div>
+    </div>
+@endsection
